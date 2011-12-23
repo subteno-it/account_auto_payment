aa8300116117a766f5e5ae468a17704ece14e864
Jean-Sebastien SUZANNE <jean-sebastien.suzanne@syleam.fr>
2011-12-23 20:20:53 +0100

    [FIX] access rule

--- __terp__.py
+++ __terp__.py
@@ -36,7 +36,7 @@
     'init_xml': [],
     'update_xml': [
         #'security/groups.xml',
-        #'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'view/menu.xml',
         'view/account_view.xml',
         #'wizard/wizard.xml',
